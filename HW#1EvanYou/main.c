3e7676f0160a4e74cc5877999733d9a6a33eabc7
Evan <evanyou2004@gmail.com>
2023-01-09 14:31:01 -0600

    Initial Commit

---
+++ HW#1EvanYou/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HW#1EvanYou
+//
+//  Created by Evan You on 1/9/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
