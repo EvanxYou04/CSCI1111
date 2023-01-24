6506b15b1577f3a40c2786a7d51ca1e492bf65aa
Evan <evanyou2004@gmail.com>
2023-01-24 17:10:48 -0600

    Stuff

---
+++ HW#1EvanYou/HW1EvanYou.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  HW#1EvanYou
+//
+//  Created by Evan You on 1/9/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Evan You\n");
+    printf("Evan \nYou\n");
+    printf("Evan");
+    printf(" You\n");
+    
+}
+/*
+ Output:
+ Evan You
+ Evan
+ You
+ Evan You
+ Program ended with exit code: 0
+ */
