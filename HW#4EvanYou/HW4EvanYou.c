7e9b5faf40cd0e0677b9f908114a0639a3a755c1
Evan <evanyou2004@gmail.com>
2023-01-31 17:51:44 -0600

    Completed Homework#4.

--- HW#4EvanYou/HW4EvanYou.c
+++ HW#4EvanYou/HW4EvanYou.c
@@ -1,15 +1,37 @@
+//HW#4EvanYou
+
 #include <stdio.h>
 #define LENGTH 50
+#define INCH_PER_FEET 12
 
 int main(int argc, char *argv[]){
-    int height; 
+    int height,feet,inches;
+    int count = 0; 
     char firstName[LENGTH];
-    printf("Enter your first name and your height in inches: ");
-    scanf("%s%i", firstName, &height);
-    while (1)
+    printf("Evan You Homework#4.\n");
+    printf("Enter your first name and your height in inches(q to exit): ");
+    while (scanf("%s%i", firstName, &height)>1)
     {
-        break;
+        feet = height / INCH_PER_FEET;
+        inches = height % INCH_PER_FEET;
+        printf("%s's height is %d feet %d inches.\n", firstName, feet, inches);
+        count++;
+        printf("Enter your first name and your height in inches(q to exit): ");
     }
-    
+    printf("Done! Entered %d sets of name(s) and height(s).\n", count);
+    return 0;
 
 }
+
+/*
+Output:
+Evan You Homework#4.
+Enter your first name and your height in inches(q to exit): Evan 67
+Evan's height is 5 feet 7 inches.
+Enter your first name and your height in inches(q to exit): Lindsay 63
+Lindsay's height is 5 feet 3 inches.
+Enter your first name and your height in inches(q to exit): q q
+Done! Entered 2 sets of name(s) and height(s).
+
+
+*/
