820613b7edd1395accbd9fa87ce658cdbb9661eb
Evan <evanyou2004@gmail.com>
2023-02-20 18:27:19 -0600

    started HW 7

---
+++ HW#7EvanYou/HWEvanYou.c
@@ -0,0 +1,17 @@
+//HW#7EvanYou.c
+#include <stdio.h>
+
+//Function Prototypes
+int get_choice(void);
+int get_first(void);
+double multiply(double i, double n);
+double divide(double i, double n);
+
+
+
+int main(void)
+{
+    printf("HW#7 by Evan You\n");
+
+    return 0;
+}
