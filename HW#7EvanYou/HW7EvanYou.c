e0770ea20ce7ea309ea86b168e65be6ca2379d1c
Evan <evanyou2004@gmail.com>
2023-03-02 18:57:53 -0600

    implemented get_first and started get_choice function

--- HW#7EvanYou/HW7EvanYou.c
+++ HW#7EvanYou/HW7EvanYou.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 //Function Prototypes
-int get_choice(void);
-int get_first(void);
+char get_choice(void);
+char get_first(void);
 double multiply(double i, double n);
 double divide(double i, double n);
-
+double add(double i, double n);
+double subtract(double i, double n);
 
 
 int main(void)
@@ -14,4 +15,33 @@ int main(void)
     printf("HW#7 by Evan You\n");
 
     return 0;
+}
+
+//function definition
+char get_first(){
+    int ch;
+    
+    ch = getchar();
+    while ( getchar() != '\n')
+        continue;
+    return ch;
+
+
+}
+char get_choice(void){
+    int ch;
+
+    printf("Please enter the letter of your choice: \n");
+    printf("a.addition      s.subtraction\n");
+    printf("m.multiplication        d.division\n");
+    printf("q.quit                             \n");
+    ch = get_first();
+    while ( ch == 'a')
+    {
+        /* code */
+    }
+    
+}
+double add(double i, double n){
+    return i + n;
 }
