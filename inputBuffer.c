c0d53aacd8d742919bcbb66789975cd960190fde
Evan <evanyou2004@gmail.com>
2023-03-21 21:53:07 -0500

    completed hw7 (late)

---
+++ inputBuffer.c
@@ -0,0 +1,8 @@
+//
+//  inputBuffer.c
+//  HWEvanYou
+//
+//  Created by Evan You on 3/21/23.
+//
+
+#include "inputBuffer.h"
---
+++ inputBuffer.h
@@ -0,0 +1,13 @@
+//
+//  inputBuffer.h
+//  HWEvanYou
+//
+//  Created by Evan You on 3/21/23.
+//
+
+#ifndef inputBuffer_h
+#define inputBuffer_h
+
+#include <stdio.h>
+
+#endif /* inputBuffer_h */
